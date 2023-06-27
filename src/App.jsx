63f95f5aac8e099dfa09220576140447925d3747
Aakanksha <tasaiaakanksha851@gmail.comgit config --global user.email tasaiaakanksha851@gmail.com>
2023-06-27 13:07:26 +0530

    online

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import Display from './components/display/display'
+
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Display/>
+    <Footer/>
+    
+    
+    </>
+  )
+}
+
+export default App
