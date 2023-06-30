897b25f7b0e64baf51b7a2d5130d776e61a138b9
Aakanksha <tasaiaakanksha851@gmail.comgit config --global user.email tasaiaakanksha851@gmail.com>
2023-06-30 13:00:25 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import Display from './components/display/display'
+import Maincard from './components/card section/Maincard'
+import Card from './components/card section/card'
 
 
 const App = () => {
   return (
     <>
     <Header/>
+    <Maincard/>
     <Display/>
     <Footer/>
     
---
+++ src/components/card section/Maincard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import Card from './card'
+
+
+const Maincard = () => {
+  return (
+
+    <div className='w-full flex h-screen overflow-y-scroll gap-6 flex-wrap justify-evenly items-center'>
+
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+
+    </div>
+
+    
+  
+
+
+  )
+}
+
+export default Maincard
